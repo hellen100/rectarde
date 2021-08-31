f1bdf39b263ab97a91dd50161609f38240ccf202
leticia <lethy.ribi15@gmail.com>
2021-08-31 14:56:15 -0300

    sala

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function HOME(){
+    return {
+        <>
+        <div>
+            <p>Seja bem-vindo</p>
+            <p> Rece</p>
+
+        </div>
+            
+        <div>
+             foto 
+        </div>
+    }
+}
+export default HOME
