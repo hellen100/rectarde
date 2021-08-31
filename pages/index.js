c33379f8d683254ba2e4297cf872b55bdc9f2a37
leticia <lethy.ribi15@gmail.com>
2021-08-31 15:05:43 -0300

    entrada

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Foto from '../imagens/foto1.png'
+
 function HOME(){
-    return {
+
+    return (
         <>
         <div>
-            <p>Seja bem-vindo</p>
-            <p> Rece</p>
+            <p>Seja ben-vindo</p>
+            <p>Receitas rápidas</p>
+            <br />
+            <p>Site com receitas rápidas e divertidas</p>
+            <p>Mata a fome e deixa você triste.</p>
+            <br />
+            <br />
+            <Link href="https://www.google.com"><button>CLIQUE PARA COZINHAR</button></Link>
 
         </div>
-            
+
         <div>
-             foto 
+            <Image src={Foto}></Image>
         </div>
-    }
+        
+        </>
+    )
 }
+
 export default HOME
